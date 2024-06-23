bd71a153c693898769703ae7f582694e662f0e2c
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-06-23 09:18:38 -0400

    improving code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
-import { Toaster } from '@/components/ui/toast';
-import WheelDashboard from './components/WheelDashboard.vue';
-import GameOverDialog from './components/GameOverDialog.vue';
-import { ref } from 'vue';
-
-const componentKey = ref(0);
-
-const updateComponentKey = (newValue: number) => {
-  componentKey.value = newValue;
-};
-
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <Toaster />
-  <GameOverDialog :componentKey="componentKey" @update:componentKey="updateComponentKey" />
-  <WheelDashboard :key="componentKey" />
-
+  <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <template>
-    <div></div>
+    <main>
+        <Toaster />
+        <GameOverDialog :componentKey="componentKey" @update:componentKey="updateComponentKey" />
+        <WheelDashboard :key="componentKey" />
+    </main>
 </template>
 
 <script setup lang="ts">
+import { Toaster } from '@/components/ui/toast';
+import WheelDashboard from '@/components/WheelDashboard.vue';
+import GameOverDialog from '@/components/GameOverDialog.vue';
+import { ref } from 'vue';
+
+const componentKey = ref(0);
+
+const updateComponentKey = (newValue: number) => {
+    componentKey.value = newValue;
+};
+
 </script>
